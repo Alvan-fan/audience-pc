64e280bfe00ef2363ff72910f78a172342589878
樊翔宇 <fanxiangyu66@163.com>
2022-01-17 20:32:00 +0800

    fix: 新增pm2配置文件

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,4 +2,4 @@ export const GA_TRACKING_ID = 'UA-206615917-1';
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const STRIPE_KEY = process.env.NEXT_PUBLIC_STRIPE_KEY;
 export const TEST_DOMAIN = 'http://35.87.142.233:3000/';
-export const REDIRECT_DOMAIN = 'http://35.87.142.233:5000/';
+export const REDIRECT_DOMAIN = 'http://35.87.142.233:5000';
