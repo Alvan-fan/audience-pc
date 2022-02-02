e5201c891a0866d2160a86b9a58508e34737ba59
樊翔宇 <fanxiangyu66@163.com>
2022-02-02 17:34:41 +0800

    fix: 跳转域名更换

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 export const GA_TRACKING_ID = 'UA-206615917-1';
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const STRIPE_KEY = process.env.NEXT_PUBLIC_STRIPE_KEY;
-export const TEST_DOMAIN = 'http://35.87.142.233:3000/';
-export const REDIRECT_DOMAIN = 'http://35.87.142.233:5000';
+export const TEST_DOMAIN = 'http://audiencepc-env.eba-em8xhnpd.us-west-2.elasticbeanstalk.com/';
+export const REDIRECT_DOMAIN = 'http://audiencenext-env.eba-ew6j5nbe.us-west-2.elasticbeanstalk.com';
